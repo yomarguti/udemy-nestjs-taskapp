eb49cb0d11ea966f4bd36c5d88660a581df82a51
Yomar Gutierrez <yomar.guti@gmail.com>
2021-11-29 16:56:44 -0500

    Frontend application and deployment

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
+  PORT: Joi.number().default(3000),
   STAGE: Joi.string().required(),
   DATABASE_HOST: Joi.string().required(),
   DATABASE_PORT: Joi.number().default(5432).required(),
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { TransformInterceptors } from './transform.interceptor';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptors());
-  await app.listen(3000);
-  logger.log(`Application running on port 3000`);
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
